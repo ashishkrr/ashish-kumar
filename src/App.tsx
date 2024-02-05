b38f3a4924611b6d650a4b84bd9b465cb9e49f92
ashwaraj <35664866+ashishkrr@users.noreply.github.com>
2024-02-05 15:27:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Landing from "./pages/Landing";
-import { HashRouter, Link, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
