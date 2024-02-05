197da0fa09f53b01bdcd1ec4d8d7e20516ad732a
ashwaraj <35664866+ashishkrr@users.noreply.github.com>
2024-02-05 15:25:05 +0530

    fixed in-page navs

--- src/components/common/UnderlinedText.tsx
+++ src/components/common/UnderlinedText.tsx
@@ -1,46 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { HashLink as Link } from 'react-router-hash-link';
 
 interface UnderlinedTextProps {
   text: string;
   color?: string;
   fontFamily?: string;
   fontSize?: string;
-  underlinePosition: string
+  underlinePosition: string;
   tag: keyof JSX.IntrinsicElements;
-  margin?: string
+  margin?: string;
 }
 
-const UnderlinedText: React.FC<UnderlinedTextProps> = ({ text, color, fontFamily, fontSize, tag, underlinePosition, margin }) => {
+const UnderlinedText: React.FC<UnderlinedTextProps> = ({
+  text,
+  color,
+  fontFamily,
+  fontSize,
+  tag,
+  underlinePosition,
+  margin,
+}) => {
   const [isHovered, setIsHovered] = useState(false);
   const style: React.CSSProperties = {
-    color: color || 'white',
-    fontFamily: 'Roboto Slab',
-    fontSize: fontSize || '1.9rem',
-    padding: '0.5rem',
-    textDecoration: 'underline',
-    textUnderlineOffset: '0.45rem' || underlinePosition,
-    WebkitTextDecorationLine: 'underline',
-    margin: margin
+    color: color || "white",
+    fontFamily: "Roboto Slab",
+    fontSize: fontSize || "1.9rem",
+    padding: "0.5rem",
+    textDecoration: "underline",
+    textUnderlineOffset: "0.45rem" || underlinePosition,
+    WebkitTextDecorationLine: "underline",
+    margin: margin,
   };
 
   const hoverStyle: React.CSSProperties = {
-    textDecorationColor: isHovered ? '#00A6EB' : '#2C2F3F',
-    cursor: isHovered ? 'pointer' : 'default'
+    textDecorationColor: isHovered ? "#00A6EB" : "#2C2F3F",
+    cursor: isHovered ? "pointer" : "default",
   };
 
   const combinedStyle = { ...style, ...hoverStyle };
 
-  return React.createElement(
-    tag,
-    {
-      href: `#${text.toLowerCase()}`,
-      style: combinedStyle,
-      onMouseEnter: () => setIsHovered(true),
-      onMouseLeave: () => setIsHovered(false),
-    },
-    text
+  return (
+    <Link
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={combinedStyle}
+      smooth
+      to={`#${text.toLowerCase()}`}
+    >
+      {text}
+    </Link>
   );
 };
 
 export default UnderlinedText;
-
